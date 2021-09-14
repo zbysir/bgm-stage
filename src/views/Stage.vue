846bc0ae5adb7b8be10f89fbcf440e3a8d62e201
zhangliang <zbysir@qq.com>
2021-09-14 23:12:11 +0800

    优化代码

--- src/views/Stage.vue
+++ src/views/Stage.vue
@@ -71,6 +71,40 @@ function MoveIt(base) {
   }
 }
 
+function AudioX(file, volume) {
+  let moveIt = MoveIt(volume)
+  let audioDom = new Audio(file)
+  audioDom.volume = volume
+
+  return {
+    play() {
+      audioDom.play()
+    },
+    pause() {
+      audioDom.pause()
+    },
+    getPaused() {
+      return audioDom.paused
+    },
+    setVolume(v, cb) {
+      moveIt.to(v, 600, (p) => {
+        audioDom.volume = p / 100
+        if (p === v) {
+          cb && cb()
+        }
+      })
+    },
+    stop() {
+      this.setVolume(0, () => {
+        audioDom.pause()
+      })
+    },
+    setLoop(loop) {
+      audioDom.loop = loop
+    }
+  }
+}
+
 export default {
   name: 'Stage',
   props: {
@@ -84,7 +118,6 @@ export default {
       playStatus: 'pause',
       currStateIndex: -1,
       audioDom: null,
-      moveIt: MoveIt(0),
     }
   },
   methods: {
@@ -95,102 +128,62 @@ export default {
 
       this.playState(this.currStateIndex - 1)
     },
-    pause(p) {
-      if (this.currStateIndex < 0) {
-        this.playState(0)
-        return
-      }
-      if (p !== undefined) {
-        if (p) {
-          this.audioDom.play()
-          this.playStatus = 'play'
-        } else {
-          this.audioDom.pause()
-          this.playStatus = 'pause'
-        }
-
-        return;
-      }
-
-      if (this.audioDom.paused) {
-        this.audioDom.play()
-        this.playStatus = 'play'
-      } else {
-        this.audioDom.pause()
-        this.playStatus = 'pause'
-      }
-    },
     moveNext() {
       if (this.currStateIndex >= this.stage.state.length - 1) {
         return
       }
       this.playState(this.currStateIndex + 1)
     },
+    play() {
+      if (this.currStateIndex < 0) {
+        this.playState(0)
+        return
+      }
+
+      this.audioDom.play()
+      this.playStatus = 'play'
+    },
+    pause() {
+      this.audioDom.pause()
+      this.playStatus = 'pause'
+    },
     async playState(index) {
       let nowState = this.stage.state[this.currStateIndex] || {}
       let wantState = this.stage.state[index]
       this.currStateIndex = index
-      console.log('nowState', nowState, wantState)
-
-      this.audioDom.loop = wantState.loop
 
       // 调整音量
       if (nowState.music_file === wantState.music_file) {
         if (nowState.volume === wantState.volume) {
           return
         }
-
-        this.moveIt.to(wantState.volume, 800, (p) => {
-              console.log('to', p)
-              this.audioDom.volume = p / 100
-            }
-        )
+        this.audioDom.setVolume(wantState.volume)
       } else {
         // 是否在播放，如果在播放则先减为 0
         if (nowState.music_file) {
-          this.moveIt.to(0, 1, (p) => {
-                console.log('to', p)
-                this.audioDom.volume = p / 100
+          this.audioDom.stop()
 
-                if (p === 0) {
-                  this.audioDom.src = wantState.music_file
-                  this.audioDom.play()
-
-                  this.moveIt.to(wantState.volume, 400, (p) => {
-                        console.log('to 2', p)
-                        this.audioDom.volume = p / 100
-                      }
-                  )
-                }
-              }
-          )
+          this.audioDom = AudioX(wantState.music_file, 0)
+          this.audioDom.play()
+          this.audioDom.setVolume(wantState.volume)
         } else {
           // 如果没有播放，则直接从音量 0 开始播放
-          this.audioDom.volume = 0
-          this.audioDom.src = wantState.music_file
+          this.audioDom = AudioX(wantState.music_file, 0)
           this.audioDom.play()
-
-          this.moveIt.setBase(0).to(wantState.volume, 400, (p) => {
-                console.log('to', p)
-                this.audioDom.volume = p / 100
-              }
-          )
+          this.audioDom.setVolume(wantState.volume)
         }
-
       }
+
+      this.play()
+      this.audioDom.setLoop(wantState.loop)
     }
   },
   async mounted() {
-    this.audioDom = new Audio("");
-
     let stage = await fetch('demo-stage.json')
     stage = await stage.json()
     this.stage = stage
 
-    // this.playState(0)
-
     // 监听 key 事件
-
     document.onkeydown = (e) => {
       let key = e.code;
       console.log('keydown', e.code)
@@ -199,14 +192,22 @@ export default {
       } else if (key === 'ArrowLeft') {
         this.movePrev()
       } else if (key === 'Space') {
-        this.pause()
+        if (!this.audioDom) {
+          this.play()
+          return
+        }
+
+        if (this.audioDom.getPaused()) {
+          this.play()
+        } else {
+          this.pause()
+        }
       }
     }
   },
   beforeDestroy() {
     if (this.audioDom){
-      this.audioDom.currentTime = 0
-      this.audioDom.pause()
+      this.audioDom.stop()
       delete this.audioDom
     }
   }
@@ -227,6 +228,7 @@ export default {
     background: #fff;
     padding: 15px;
     margin: 10px;
+    transition: all 600ms;
 
     &.playing {
       background: #adee78;
