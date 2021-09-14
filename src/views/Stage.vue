eb7ccc6db2e55b24be5e2ffa1f527a88994d5a72
zhangliang <zbysir@qq.com>
2021-09-14 13:40:08 +0800

    优化切换时音量渐入

--- src/views/Stage.vue
+++ src/views/Stage.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <div class="status">
+      <span v-if="playStatus==='play'"> 正在播放 </span>
+      <span v-else> 暂停中 </span>
+    </div>
+
     <div class="state-box">
       <div v-for="(item,index) in stage.state"
            class="state-item"
@@ -15,6 +20,55 @@
   </div>
 </template>
 <script>
+
+function MoveIt(base) {
+  let t
+  return {
+    base,
+    setBase(base) {
+      this.base = base
+      return this
+    },
+    to(to, duration, callback) {
+      if (t) {
+        clearTimeout(t)
+      }
+
+      let from = base
+
+      let step = Math.abs(to - from) / 5 + 1
+      let x = to > from ? 1 : -1
+      let stepTime = duration / step
+
+      let index = 1
+      let update = () => {
+        let want = from + (index) * 5 * x
+        if (x === 1) {
+          if (want >= to) {
+            want = to
+          }
+
+        } else {
+          if (want <= to) {
+            want = to
+          }
+
+        }
+        base = want
+        callback(want)
+
+        index += 1
+        if (index >= step) {
+          return
+        }
+        t = setTimeout(update, stepTime)
+      }
+
+      t = setTimeout(update, stepTime)
+    }
+  }
+}
+
 export default {
   name: 'Stage',
   props: {
@@ -25,8 +79,10 @@ export default {
       stage: {
         state: []
       },
+      playStatus: 'pause',
       currStateIndex: -1,
       audioDom: null,
+      moveIt: MoveIt(0),
     }
   },
   methods: {
@@ -37,51 +93,37 @@ export default {
 
       this.playState(this.currStateIndex - 1)
     },
+    pause(p) {
+      if (this.currStateIndex < 0) {
+        this.playState(0)
+        return
+      }
+      if (p !== undefined) {
+        if (p) {
+          this.audioDom.play()
+          this.playStatus = 'play'
+        } else {
+          this.audioDom.pause()
+          this.playStatus = 'pause'
+        }
+
+        return;
+      }
+
+      if (this.audioDom.paused) {
+        this.audioDom.play()
+        this.playStatus = 'play'
+      } else {
+        this.audioDom.pause()
+        this.playStatus = 'pause'
+      }
+
+    },
     moveNext() {
       if (this.currStateIndex >= this.stage.state.length - 1) {
         return
       }
       this.playState(this.currStateIndex + 1)
-    },
-    // todo 取消掉上一次 transform
-    transform(aStage, bStage, duration) {
-      return new Promise(resolve => {
-        let from = aStage.volume
-        let to = bStage.volume
-
-        let step = Math.abs(to - from) / 5 + 1
-        let x = to > from ? 1 : -1
-        let stepTime = duration / step
-
-        console.log('step', step, stepTime)
-
-        let index = 1
-
-        let upVolume = () => {
-          let want = from + (index) * 5 * x
-          if (x === 1) {
-            if (want >= to) {
-              want = to
-            }
-
-          } else {
-            if (want <= to) {
-              want = to
-            }
-
-          }
-          this.audioDom.volume = want / 100
-          console.log('up', want)
-          index += 1
-          if (index >= step) {
-            resolve()
-            return
-          }
-          setTimeout(upVolume, stepTime)
-        }
-
-        setTimeout(upVolume, stepTime)
-      })
     },
     async playState(index) {
       let nowState = this.stage.state[this.currStateIndex] || {}
@@ -89,23 +131,43 @@ export default {
       this.currStateIndex = index
       console.log('nowState', nowState, wantState)
 
+      this.audioDom.loop = wantState.loop
+
+      // 调整音量
       if (nowState.music_file === wantState.music_file) {
         if (nowState.volume === wantState.volume) {
           return
         }
 
-        // 调整音量
-        // this.audioDom.volume = wantState.volume / 100
-
-        await this.transform(nowState, wantState, 800)
-        console.log("down")
+        this.moveIt.to(wantState.volume, 800, (p) => {
+              console.log('to', p)
+              this.audioDom.volume = p / 100
+            }
+        )
       } else {
+        // 是否在播放，如果在播放则先减为 0
+        if (nowState.music_file) {
+          this.moveIt.to(0, 400, (p) => {
+                console.log('to', p)
+                this.audioDom.volume = p / 100
+              }
+          )
+
+          await new Promise(resolve => {
+            setTimeout(resolve, 400)
+          })
+        }
+
+        this.audioDom.volume = 0
         this.audioDom.src = wantState.music_file
+
         this.audioDom.play()
-        this.audioDom.volume = wantState.volume / 100
+        this.moveIt.to(wantState.volume, 400, (p) => {
+              console.log('to', p)
+              this.audioDom.volume = p / 100
+            }
+        )
       }
-
-
     }
   },
   async mounted() {
@@ -115,7 +177,7 @@ export default {
     stage = await stage.json()
     this.stage = stage
 
-    this.playState(0)
+    // this.playState(0)
 
     // 监听 key 事件
 
@@ -126,6 +188,8 @@ export default {
         this.moveNext()
       } else if (key === 'ArrowLeft') {
         this.movePrev()
+      } else if (key === 'Space') {
+        this.pause()
       }
     }
   },
